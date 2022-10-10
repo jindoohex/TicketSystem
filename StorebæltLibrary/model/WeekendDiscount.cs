98630154b212599b6e1a4f1bf4379f040a80ae61
Alex G. Jensen <jindoohex@hotmail.com>
2022-10-10 02:37:22 +0200

    Added weekend discount + unit test

--- StorebæltLibrary/model/WeekendDiscount.cs
+++ StorebæltLibrary/model/WeekendDiscount.cs
@@ -3,11 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketLibrary.model;
 
 namespace StorebæltLibrary.model
 {
     public class WeekendDiscount
     {
+        /// <summary>
+        /// Initializing objects from the external DLLs
+        /// </summary>
+        private Car car = new();
+        private Motorcycle mc = new();
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Discount { get; set; }
+        public DateTime date { get; set; }
+
+
+        /// <summary>
+        /// Default constructor for the weekend discount
+        /// </summary>
+        public WeekendDiscount()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Parameterized constructor for the weekend discount
+        /// </summary>
+        /// <param name="discount"></param>
+        public WeekendDiscount(double discount)
+        {
+            Discount = discount;
+        }
+
+        /// <summary>
+        /// This method calculates the wekeend discount based on weekend days
+        /// </summary>
+        /// <param name="discount">Discount of type: double</param>
+        /// <returns>Returns the discounted price if day of the week is on a weekend day</returns>
+        public double CarStorebæltWeekendDiscount()
+        {
+            //DayOfWeek dw = DateTime.Today.DayOfWeek;
+            double discountedPrice = car.VehicleTicketPrice();
+
+            bool saturday = true;
+            bool sunday = true;
+
+            if (saturday == true)
+            {
+                return car.VehicleTicketPrice() * 0.80;
+            }
+
+            if (sunday == true)
+            {
+                return car.VehicleTicketPrice() * 0.80;
+            }
+
+            //if (dw == DayOfWeek.Saturday || dw == DayOfWeek.Sunday)
+            //{
+            //    car.VehicleTicketPrice() * 0.80;
+            //}
+            
+            //if (dw == DayOfWeek.Saturday || dw == DayOfWeek.Sunday)
+            //{
+            //    car.VehicleTicketPrice() * 0.80;
+            //}
+            return discountedPrice;
+        }
     }
 }
---
+++ TicketSystemTest/LibraryTest/StorebæltLibraryTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorebæltLibrary.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketLibrary.model;
+
+namespace TicketSystemTest.LibraryTest
+{
+    [TestClass()]
+    public class StorebæltLibraryTest
+    {
+        /// <summary>
+        /// Initializing the vehicle objects from DLL + WeekendDiscount object
+        /// </summary>
+        private Car car = new();
+        private Motorcycle mc = new();
+        private WeekendDiscount weekendDiscount = new();
+
+        [TestMethod()]
+        public void Storebælt_Should_Discount_20Percent_From_Ticket_On_Weekends()
+        {
+            // Arrange
+            double expectedValue = 192;
+
+            // Act
+            double actualValue = weekendDiscount.CarStorebæltWeekendDiscount();
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue, 0.01);
+        }
+
+        [TestMethod()]
+        public void Storebælt_Discount_And_Brobizz()
+        {
+            // Arrange
+            double expectedValue = 182.4;
+
+            // Act
+            double actualValue = weekendDiscount.CarStorebæltWeekendDiscount() * 0.95;
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue, 0.01);
+        }
+    }
+}
